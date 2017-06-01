17c90324e8331e231e73bcf37b93127eeb192e08
Sergey S <volshibenik@gmail.com>
2017-06-01 17:07:26 +0300

    cool task

---
+++ intermediate/7.DNApairs.js
@@ -0,0 +1,33 @@
+/*
+                    DNA Pairing
+
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+*/
+
+
+function pairElement(str) {
+  var arr = str.split('');
+  var newArr = arr.map(function(val){
+    
+    switch(val) {
+      case "C": val+="G"; break;
+      case "G": val+="C"; break;
+      case "A": val+="T"; break;
+      case "T": val+="A"; break;
+    }
+    
+    return val.split('');
+  });
+  
+  return newArr;
+}
+
+console.log(pairElement("GCG"));
